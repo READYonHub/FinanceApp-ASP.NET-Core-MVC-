2162b86cc3536087c98ddba333c713bc9cb77b03
READYonHub <readyemail3@gmail.com>
2025-02-25 12:42:07 +0100

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        public double Amount{ get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage ="Az összegnek többnek kell lennie 0-nál!")]
+        public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
